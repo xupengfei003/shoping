da6ebff90e6f5bc2dea613d6b53534a2ef7d074d
hc <chen.huang@sao.so>
2017-07-24 21:30:46 +0800

    修改配置

--- src/main/java/so/sao/shop/supplier/Application.java
+++ src/main/java/so/sao/shop/supplier/Application.java
@@ -1,11 +1,11 @@
-package so.sao.shop;
+package so.sao.shop.supplier;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.sao.so.supplier.dao")
+@MapperScan("so.sao.shop.supplier.dao")
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
