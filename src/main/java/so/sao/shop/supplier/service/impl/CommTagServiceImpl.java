5eb7f1d307355012a49c895985c12cbb25d258d4
yan.zhao <yan.zhao@sao.so>
2017-08-31 16:02:04 +0800

    商品标签模块代码优化

--- src/main/java/so/sao/shop/supplier/service/impl/CommTagServiceImpl.java
+++ src/main/java/so/sao/shop/supplier/service/impl/CommTagServiceImpl.java
@@ -1,5 +1,7 @@
 package so.sao.shop.supplier.service.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -7,11 +9,10 @@ import so.sao.shop.supplier.config.Constant;
 import so.sao.shop.supplier.dao.CommTagDao;
 import so.sao.shop.supplier.dao.SupplierCommodityDao;
 import so.sao.shop.supplier.domain.CommTag;
-import so.sao.shop.supplier.domain.SupplierCommodity;
 import so.sao.shop.supplier.pojo.Result;
 import so.sao.shop.supplier.pojo.input.CommTagUpdateInput;
 import so.sao.shop.supplier.service.CommTagService;
-import so.sao.shop.supplier.util.Ognl;
+import so.sao.shop.supplier.util.BeanMapper;
 
 import java.util.Date;
 import java.util.List;
@@ -30,6 +31,11 @@ public class CommTagServiceImpl implements CommTagService {
     private SupplierCommodityDao supplierCommodityDao;
 
     /**
+     * 初始化日志
+     */
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    /**
      * 新增商品标签
      *
      * @param supplierId 供应商ID
@@ -39,41 +45,19 @@ public class CommTagServiceImpl implements CommTagService {
     @Override
     @Transactional(rollbackFor = Exception.class)
     public Result saveCommTag(Long supplierId, String name) {
-        //方法返回对象
-        Result result = new Result();
-        //判断标签名为null或空
-        if (null == name || Ognl.isEmpty(name.trim())) {
-            result.setCode(so.sao.shop.supplier.config.Constant.CodeConfig.CODE_FAILURE);
-            result.setMessage("请输入商品标签名称！");
-            return result;
-        }
-        CommTag commTag = new CommTag();
-        commTag.setName(name.trim());
-        commTag.setSupplierId(supplierId);
-        commTag.setCreatedAt(new Date());
-        commTag.setUpdatedAt(new Date());
-        //校验商品标签名称长度
-        if (name.trim().length() > Constant.CheckMaxLength.MAX_TAG_NAME_LENGTH) {
-            result.setCode(so.sao.shop.supplier.config.Constant.CodeConfig.CODE_FAILURE);
-            result.setMessage("商品标签名称长度不能超过" + Constant.CheckMaxLength.MAX_TAG_NAME_LENGTH + "位！");
-            return result;
-        }
-        //判断新增标签名余supplierId是否重复,不重复时进行新增，重复时返回重复信息
-        List<CommTag> list = commTagDao.findByNameAndSupplierId(name.trim(), supplierId);
-        if (null != list && list.size() == 0) {
-            //判断新增成功或失败
-            if (commTagDao.save(commTag)) {
-                result.setCode(Constant.CodeConfig.CODE_SUCCESS);
-                result.setMessage("商品标签新增成功！");
-            } else {
-                result.setCode(Constant.CodeConfig.CODE_FAILURE);
-                result.setMessage("商品标签新增失败！");
-            }
+        //判断新增标签名与supplierId是否重复,不重复时进行新增，重复时返回重复信息
+        Result result = checkCommTagName(supplierId, name.trim());
+        if (null != result) {
             return result;
         } else {
-            result.setCode(Constant.CodeConfig.CODE_FAILURE);
-            result.setMessage("商品标签新增失败，新增的商品标签已存在！");
-            return result;
+            CommTag commTag = new CommTag();
+            commTag.setName(name.trim());
+            commTag.setSupplierId(supplierId);
+            commTag.setCreatedAt(new Date());
+            commTag.setUpdatedAt(new Date());
+            //新增操作
+            commTagDao.save(commTag);
+            return Result.success("商品标签新增成功！");
         }
     }
 
@@ -87,38 +71,19 @@ public class CommTagServiceImpl implements CommTagService {
     @Override
     @Transactional(rollbackFor = Exception.class)
     public Result deleteCommTag(Long supplierId, Long id) {
-        //方法返回对象
-        Result result = new Result();
-        //判断该ID对应的商品标签是否存在
-        CommTag commTag = commTagDao.findOne(id);
-        if (null == commTag) {
-            result.setCode(Constant.CodeConfig.CODE_FAILURE);
-            result.setMessage("商品标签删除失败，该标签不存在！");
-            return result;
-        }
-        //判断ID对应的商品标签supplierId与登录用户supplierId是否相同
-        if (supplierId.longValue() != commTag.getSupplierId().longValue()) {
-            result.setCode(Constant.CodeConfig.CODE_FAILURE);
-            result.setMessage("您无权删除此商品标签！");
+        //商品标签是否存在与操作权限判断
+        Result result = checkExistAndPermission(supplierId, id);
+        if (null != result){
             return result;
         }
         //根据该ID在供应商商品表中查询是是否在被使用（未删除状态），在使用状态时不能进行删除操作
-        List<SupplierCommodity> supplierCommodityList = supplierCommodityDao.findSupplierCommodityByTagId(id);
-        if (null != supplierCommodityList && supplierCommodityList.size() > 0) {
-            result.setCode(Constant.CodeConfig.CODE_FAILURE);
-            result.setMessage("商品标签正在使用，暂时无法删除此标签！");
-            return result;
+        int count = supplierCommodityDao.countSupplierCommodityByTagId(id);
+        if (count > 0) {
+            return Result.fail("商品标签正在使用，暂时无法删除此标签！");
         }
-        //判断删除操作成功或失败
-        if (commTagDao.deleteById(id)) {
-            result.setCode(Constant.CodeConfig.CODE_SUCCESS);
-            result.setMessage("商品标签删除成功！");
-            return result;
-        } else {
-            result.setCode(Constant.CodeConfig.CODE_FAILURE);
-            result.setMessage("商品标签删除失败！");
-            return result;
-        }
+        //删除操作
+        commTagDao.deleteById(id);
+        return Result.success("商品标签删除成功！");
     }
 
     /**
@@ -131,49 +96,22 @@ public class CommTagServiceImpl implements CommTagService {
     @Override
     @Transactional(rollbackFor = Exception.class)
     public Result updateCommTag(Long supplierId, CommTagUpdateInput commTagUpdateInput) {
-        //方法返回对象
-        Result result = new Result();
-        CommTag commTag = new CommTag();
-        commTag.setId(commTagUpdateInput.getId());
-        commTag.setName(commTagUpdateInput.getName().trim());
-        commTag.setSupplierId(supplierId);
-        commTag.setUpdatedAt(new Date());
-        //判断标签名为null或空
-        if (Ognl.isEmpty(commTagUpdateInput.getName().trim())) {
-            result.setCode(so.sao.shop.supplier.config.Constant.CodeConfig.CODE_FAILURE);
-            result.setMessage("请输入商品标签名称！");
-            return result;
-        }
-        //判断该ID对应的商品标签是否存在
-        CommTag one = commTagDao.findOne(commTagUpdateInput.getId());
-        if (null == one) {
-            result.setCode(Constant.CodeConfig.CODE_FAILURE);
-            result.setMessage("商品标签修改失败，该标签不存在！");
-            return result;
-        }
-        //判断该ID对应的商品标签supplierId与登录用户supplierId是否相同
-        if (supplierId.longValue() != one.getSupplierId().longValue()) {
-            result.setCode(Constant.CodeConfig.CODE_FAILURE);
-            result.setMessage("您无权修改此商品标签！");
+        //商品标签是否存在与操作权限判断
+        Result result = checkExistAndPermission(supplierId, commTagUpdateInput.getId());
+        if (null != result){
             return result;
         }
         //根据修改的name和supplierId判断是否重复
-        List<CommTag> list = commTagDao.findByNameAndSupplierId(commTagUpdateInput.getName().trim(), supplierId);
-        if (null != list && list.size() > 0) {
-            result.setCode(Constant.CodeConfig.CODE_FAILURE);
-            result.setMessage("商品标签修改失败，修改的商品标签名称重复！");
+        Result checkNameResult = checkCommTagName(supplierId, commTagUpdateInput.getName().trim());
+        if (null != checkNameResult) {
             return result;
         }
-        //判断修改标签是否成功
-        if (commTagDao.update(commTag)) {
-            result.setCode(Constant.CodeConfig.CODE_SUCCESS);
-            result.setMessage("商品标签修改成功！");
-            return result;
-        } else {
-            result.setCode(Constant.CodeConfig.CODE_FAILURE);
-            result.setMessage("商品标签修改失败！");
-            return result;
-        }
+        CommTag commTag = BeanMapper.map(commTagUpdateInput, CommTag.class);
+        commTag.setSupplierId(supplierId);
+        commTag.setUpdatedAt(new Date());
+        //进行修改操作
+        commTagDao.update(commTag);
+        return Result.success("商品标签修改成功！");
     }
 
     /**
@@ -184,18 +122,45 @@ public class CommTagServiceImpl implements CommTagService {
      */
     @Override
     public Result searchCommTag(Long supplierId) {
-        //方法返回对象
-        Result result = new Result();
+        //商品标签查询操作
         List<CommTag> commTags = commTagDao.find(supplierId);
-        //判断商品标签查询是否成功
-        if (null == commTags || commTags.size() == 0) {
-            result.setCode(Constant.CodeConfig.CODE_FAILURE);
-            result.setMessage("查询商品标签失败！");
-        } else {
-            result.setCode(Constant.CodeConfig.CODE_SUCCESS);
-            result.setMessage("查询商品标签成功！");
-            result.setData(commTags);
+        return Result.success("查询商品标签成功！",commTags);
+    }
+
+    /**
+     * 根据suppplierId与id进行存在性检验与操作权限校验
+     *
+     * @param supplierId 供应商ID
+     * @param id 商品标签ID
+     * @return 返回Result
+     */
+    private Result checkExistAndPermission(Long supplierId, Long id){
+        //商品标签存在性判断
+        CommTag commTag = commTagDao.findOne(id);
+        if (null == commTag) {
+            return Result.fail("该商品标签不存在！");
         }
-        return result;
+        //判断ID对应的商品标签supplierId与登录用户supplierId是否相同
+        if (!supplierId.equals(commTag.getSupplierId())) {
+            return Result.fail("预置商品标签无权操作！");
+        }
+        return null;
+    }
+
+    /**
+     * 根据suppplierId与name进行判断商品标签名称是否重复
+     *
+     * @param supplierId 供应商ID
+     * @param name  商品标签名称
+     * @return 返回Result
+     */
+    private Result checkCommTagName(Long supplierId, String name){
+        //判断新增标签名是否重复
+        int count = commTagDao.countByNameAndSupplierId(name.trim(), supplierId);
+        if (count > 0) {
+            return  Result.fail("商品标签已存在！");
+        } else {
+            return null;
+        }
     }
 }
