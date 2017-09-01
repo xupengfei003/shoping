cf1b283ea17a673e95c1745d2ebcdec1ca9a94ca
hengle.yang <hengle.yang@sao.so>
2017-09-01 17:24:59 +0800

    1.修改二维码前缀的配置 2.生成二维码前验证一个订单只能生成一个二维码

--- src/main/java/so/sao/shop/supplier/dao/QrcodeDao.java
+++ src/main/java/so/sao/shop/supplier/dao/QrcodeDao.java
@@ -54,4 +54,12 @@ public interface QrcodeDao {
      * @return 返回二维码对象集合
      */
     public List<Qrcode> findQrcodeByOrderId(String orderId);
+
+    /**
+     * 根据支付id查询对应订单的二维码，返回二维码数量
+     *
+     * @param payId 支付id
+     * @return
+     */
+    int getQrcodesByPayId(String payId);
 }
