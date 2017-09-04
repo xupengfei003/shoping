841017095a6d4e578f741089e378e3c448ca9cc6
pengfei.xu <pengfei.xu@sao.so>
2017-09-04 16:56:59 +0800

    优化 ：图片中文截取

--- src/main/java/so/sao/shop/supplier/service/impl/CommodityServiceImpl.java
+++ src/main/java/so/sao/shop/supplier/service/impl/CommodityServiceImpl.java
@@ -756,15 +756,7 @@ public class CommodityServiceImpl implements CommodityService {
                 break;
             case "图片":
                 if (!"".equals(value)) {
-                    String[] imgs = new String[15];
-                    if (value.contains(",") || value.contains("，") ) {
-                        if( value.contains("，") ){
-                            value = value.replaceAll("，" , ",");
-                        }
-                        imgs = value.split(",");
-                    } else {
-                        imgs[0] = value;
-                    }
+                    String[] imgs = value.replaceAll("，", ",").split(",");
                     List<String> imgList = Arrays.asList(imgs);
                     // 上传图片
                     List<Result> results = azureBlobService.uploadFilesComm(filePath, imgList);
