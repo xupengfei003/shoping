1f98d1b2f1c0815f7a7dbcb9e53a2bd98aaecc87
zhihua.bai <zhihua.bai@sao.so>
2017-09-08 16:08:20 +0800

    修改自动检索未支付超时订单

--- src/main/java/so/sao/shop/supplier/job/ScheduledJob.java
+++ src/main/java/so/sao/shop/supplier/job/ScheduledJob.java
@@ -69,7 +69,8 @@ public class ScheduledJob {
      * 定时执行取消到期待付款的订单
      * @throws Exception
      */
-//    @Scheduled(cron = "0 0/1 * * * ?")
+    //定位每30分钟检索一次待付款的订单
+//    @Scheduled(cron = "0 0/30 * * * ?")
     public void CancelOrderJob () throws Exception {
         System.out.println("11111111");
         Boolean lock = redisTemplate.opsForValue().setIfAbsent(Constant.REDIS_KEY_PREFIX + "CANCEL_PAYMENT_ORDER", "1");
