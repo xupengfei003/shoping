5c2988ccd724fc129111691096eda86e52f67ae7
yiyun.wang <yiyun.wang@sao.so>
2017-09-12 17:59:15 +0800

    修改import

--- src/main/java/so/sao/shop/supplier/web/AppCartController.java
+++ src/main/java/so/sao/shop/supplier/web/AppCartController.java
@@ -7,7 +7,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
+
 import so.sao.shop.supplier.config.Constant;
 import so.sao.shop.supplier.domain.AppCartItem;
 import so.sao.shop.supplier.domain.User;
