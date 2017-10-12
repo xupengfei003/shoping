1ad5130152cb1e293d01af61e0fdb51a37e7bed3
zhihua.bai <zhihua.bai@sao.so>
2017-10-12 17:31:13 +0800

    物流信息修改； 自配配送订单物流信息修改； 物流配送订单物流信息修改； 管理员订单列表； 管理员订单信息导出； 管理员订单详情

--- src/main/java/so/sao/shop/supplier/web/LogisticsController.java
+++ src/main/java/so/sao/shop/supplier/web/LogisticsController.java
@@ -3,12 +3,12 @@ package so.sao.shop.supplier.web;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import so.sao.shop.supplier.config.Constant;
 import so.sao.shop.supplier.pojo.Result;
 import so.sao.shop.supplier.service.LogisticsService;
+
+import java.util.Map;
 
 /**
  * Created by wyy on 2017/8/16.
@@ -25,5 +25,14 @@ public class LogisticsController {
     @GetMapping(value = "/{num}")
     public Result<Object> findLogisticInfo(@PathVariable("num")String num){
         return logisticsService.findLogisticInfo(num);
+    }
+    @ApiOperation(value = "查询物流信息" ,notes = "根据物流单号查询物流信息")
+    @PostMapping(value = "/artificialAutomaticReceive")
+    public Result artificialAutomaticReceive(String orderId) throws Exception {
+        Map<String,Object> resultMap = logisticsService.insertReceivedOrder(orderId);
+        if("fail".equals(resultMap.get("flag"))){
+            return Result.fail(Constant.MessageConfig.MSG_FAILURE,resultMap.get("msg"));
+        }
+        return Result.success(Constant.MessageConfig.MSG_SUCCESS);
     }
 }
