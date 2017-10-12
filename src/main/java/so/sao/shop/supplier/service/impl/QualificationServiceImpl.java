b8edd7c959f70c6c1e00a306afc0a70732590fd9
yabing.ma <yabing.ma@sao.so>
2017-10-12 11:11:31 +0800

    供应商资质审核： 更新资质状态和时间

--- src/main/java/so/sao/shop/supplier/service/impl/QualificationServiceImpl.java
+++ src/main/java/so/sao/shop/supplier/service/impl/QualificationServiceImpl.java
@@ -1,7 +1,15 @@
 package so.sao.shop.supplier.service.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import so.sao.shop.supplier.dao.QualificationDao;
+import so.sao.shop.supplier.pojo.Result;
 import so.sao.shop.supplier.service.QualificationService;
+import so.sao.shop.supplier.util.Ognl;
+
+import java.util.Date;
 
 /**
  * Created by liugang on 2017/10/11.
@@ -9,4 +17,46 @@ import so.sao.shop.supplier.service.QualificationService;
  */
 @Service
 public class QualificationServiceImpl implements QualificationService{
+
+    @Autowired
+    private QualificationDao qualificationDao;
+
+
+    /**
+     * 初始化日志
+     */
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    /**
+     * 资质审核 - 更新资质状态和时间
+     * @param accountId
+     * @param qualificationStatus
+     * @return Result
+     */
+    @Override
+    public Result updateQualificationStatus(Integer accountId, Integer qualificationStatus) {
+        Date updateDate = new Date();
+        try{
+            qualificationDao.updateQualificationStatus( accountId, qualificationStatus,updateDate );
+        }catch (Exception e){
+            logger.info("更新异常", e);
+            return Result.fail("更新异常");
+        }
+        return Result.success("更新成功");
+    }
+
+    /**
+     * 查询登录供应商的资质状态
+     * @param accountId
+     * @return Result
+     */
+    @Override
+    public Result getAccountQualificationStatus(Integer accountId) {
+        Integer qualificationStatus = qualificationDao.getAccountQualificationStatus( accountId );
+        if(Ognl.isNull(qualificationStatus)){
+            return Result.fail("暂无数据");
+        }
+        return Result.success("查询成功", qualificationStatus );
+    }
+
 }
--- src/main/java/so/sao/shop/supplier/web/QualificationController.java
+++ src/main/java/so/sao/shop/supplier/web/QualificationController.java
@@ -1,9 +1,10 @@
 package so.sao.shop.supplier.web;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import so.sao.shop.supplier.pojo.Result;
 import so.sao.shop.supplier.service.QualificationService;
 
 /**
@@ -16,5 +17,31 @@ public class QualificationController {
 
     @Autowired
     private QualificationService qualificationService;
-    
+
+    /**
+     * 资质审核 - 更新资质状态和时间
+     * @param accountId
+     * @param qualificationStatus
+     * @return Result
+     */
+    @ApiOperation(value = "资质审核", notes = "更新资质状态和时间【负责人：许鹏飞】")
+    @PostMapping("/updateQualificationStatus")
+    public Result updateQualificationStatus(@RequestParam("accountId") Integer accountId, @RequestParam("qualificationStatus") Integer qualificationStatus ){
+        Result result = qualificationService.updateQualificationStatus( accountId, qualificationStatus );
+        return result;
+    }
+
+    /**
+     * 查询登录供应商的资质状态
+     * @param accountId
+     * @return Result
+     */
+    @ApiOperation(value = "查询登录供应商的资质状态", notes = "查询登录供应商的资质状态【负责人：许鹏飞】")
+    @GetMapping("getAccountQualificationStatus")
+    public Result getAccountQualificationStatus(@RequestParam("accountId") Integer accountId){
+        Result result = qualificationService.getAccountQualificationStatus( accountId );
+        return result;
+    }
+
+
 }
