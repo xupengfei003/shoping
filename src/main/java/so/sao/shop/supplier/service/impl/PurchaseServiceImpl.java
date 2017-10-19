b1caa1e01d981d059c32fbc02aa6187029db000f
zhihua.bai <zhihua.bai@sao.so>
2017-10-19 09:56:44 +0800

    管理员导出bug修复

--- src/main/java/so/sao/shop/supplier/service/impl/PurchaseServiceImpl.java
+++ src/main/java/so/sao/shop/supplier/service/impl/PurchaseServiceImpl.java
@@ -462,23 +462,7 @@ public class PurchaseServiceImpl implements PurchaseService {
             cellTemp.setCellStyle(style);
             Integer status = purchase.getOrderStatus();
             String orderStatus = "";
-            if (status == Constant.OrderStatusConfig.PAYMENT) {
-                orderStatus = Constant.OrderMessageConfig.PAYMENT;
-            } else if (status == Constant.OrderStatusConfig.PENDING_SHIP) {
-                orderStatus = Constant.OrderMessageConfig.PENDING_SHIP;
-            } else if (status == Constant.OrderStatusConfig.ISSUE_SHIP || status == Constant.OrderStatusConfig.CONFIRM_RECEIVED) {
-                orderStatus = Constant.OrderMessageConfig.ISSUE_SHIP;
-            } else if (status == Constant.OrderStatusConfig.RECEIVED) {
-                orderStatus = Constant.OrderMessageConfig.RECEIVED;
-            } else if (status == Constant.OrderStatusConfig.REJECT) {
-                orderStatus = Constant.OrderMessageConfig.REJECT;
-            } else if (status == Constant.OrderStatusConfig.REFUNDED) {
-                orderStatus = Constant.OrderMessageConfig.REFUNDED;
-            } else if (status == Constant.OrderStatusConfig.CANCEL_ORDER) {
-                orderStatus = Constant.OrderMessageConfig.CANCEL_ORDER;
-            } else if (status == Constant.OrderStatusConfig.PAYMENT_CANCEL_ORDER) {
-                orderStatus = Constant.OrderMessageConfig.PAYMENT_CANCEL_ORDER;
-            }
+            orderStatus = getOrderStatusInExcel(status, orderStatus);
             cellTemp = row.createCell(4);
             cellTemp.setCellValue(orderStatus);
             cellTemp.setCellStyle(style);
@@ -516,25 +500,7 @@ public class PurchaseServiceImpl implements PurchaseService {
             cellTemp.setCellStyle(style);
         }
         //输出Excel文件
-        OutputStream output = null;
-        try {
-            output = response.getOutputStream();
-            response.reset();
-            String agent = request.getHeader("USER-AGENT").toLowerCase();
-            response.setContentType("application/vnd.ms-excel");
-            String fileName = "订单统计" + System.currentTimeMillis();
-            String codedFileName = java.net.URLEncoder.encode(fileName, "UTF-8");
-            if (agent.contains("firefox")) {
-                response.setCharacterEncoding("utf-8");
-                response.setHeader("content-disposition", "attachment;filename=" + new String(fileName.getBytes(), "ISO8859-1") + ".xls");
-            } else {
-                response.setHeader("content-disposition", "attachment;filename=" + codedFileName + ".xls");
-            }
-            wb.write(output);
-            output.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        outputExcel(request, response, wb);
     }
 
     /**
@@ -1576,5 +1542,171 @@ public class PurchaseServiceImpl implements PurchaseService {
             return false;
         }
         return true;
+    }
+
+    /**
+     * 管理员POI导出(当前页/所选页/全部)订单列表
+     *
+     * @param request   request
+     * @param response  response
+     * @param pageNum   pageNum
+     * @param accountId accountId
+     * @param pageSize  pageSize
+     */
+    @Override
+    public void adminExportExcel(HttpServletRequest request, HttpServletResponse response, String pageNum, Integer pageSize, Long accountId, PurchaseSelectInput purchaseSelectInput) throws Exception {
+
+        //创建HSSFWorkbook对象(excel的文档对象)
+        HSSFWorkbook wb = new HSSFWorkbook();
+        //声明一个单子并命名
+        HSSFSheet sheet = wb.createSheet("订单统计");
+        for (int i = 0; i < 10; i++) {
+            sheet.setColumnWidth(i, 25 * 256);
+        }
+        // 生成一个样式
+        HSSFCellStyle style = wb.createCellStyle();
+
+        //创建第一行（也可以称为表头）
+        HSSFRow row = sheet.createRow(0);
+        row.setHeightInPoints(30);
+        //样式字体居中
+        style.setAlignment(HSSFCellStyle.ALIGN_CENTER);
+        style.setVerticalAlignment(HSSFCellStyle.VERTICAL_CENTER);
+        //给表头第一行一次创建单元格
+
+        HSSFCell cell = row.createCell(0);
+        cell.setCellValue("供应商名称");
+        cell.setCellStyle(style);
+
+        cell = row.createCell(1);
+        cell.setCellValue("订单编号");
+        cell.setCellStyle(style);
+
+        cell = row.createCell(2);
+        cell.setCellValue("订单状态");
+        cell.setCellStyle(style);
+
+        cell = row.createCell(3);
+        cell.setCellValue("订单金额");
+        cell.setCellStyle(style);
+
+        cell = row.createCell(4);
+        cell.setCellValue("创建时间");
+        cell.setCellStyle(style);
+
+        cell = row.createCell(5);
+        cell.setCellValue("支付时间");
+        cell.setCellStyle(style);
+
+        cell = row.createCell(6);
+        cell.setCellValue("支付类型");
+        cell.setCellStyle(style);
+
+        cell = row.createCell(7);
+        cell.setCellValue("支付流水号");
+        cell.setCellStyle(style);
+
+        List<Purchase> orderList;
+        List<Integer> pageNumList;
+
+        if (pageNum != null && pageNum.length() > 0 && pageSize != null) { //获取区间页列表
+            //java8新特性 逗号分隔字符串转List<Integer>
+            pageNumList = Arrays.asList(pageNum.split(",")).stream().map(s -> Integer.parseInt(s.trim())).collect(Collectors.toList());
+            if (pageNumList.size() > 1) {
+                Collections.sort(pageNumList);
+                orderList = (List<Purchase>) PageTool.getListByPage(purchaseDao.getOrderListByIds(purchaseSelectInput, accountId), pageNumList.get(0), pageNumList.get(1), pageSize);
+            } else { //获取当前页列表
+                orderList = (List<Purchase>) PageTool.getListByPage(purchaseDao.getOrderListByIds(purchaseSelectInput, accountId), pageNumList.get(0), pageNumList.get(0), pageSize);
+            }
+        } else { //查询全部列表
+            orderList = purchaseDao.getOrderListByIds(purchaseSelectInput, accountId);
+        }
+        //向单元格里填充数据
+        HSSFCell cellTemp = null;
+        for (int i = 0; i < orderList.size(); i++) {
+            Purchase purchase = orderList.get(i);
+            sheet.setColumnWidth(0, 20 * 256);
+            row = sheet.createRow(i + 1);
+            cellTemp = row.createCell(0);
+            cellTemp.setCellValue(purchase.getStoreName());
+            cellTemp.setCellStyle(style);
+            cellTemp = row.createCell(1);
+            cellTemp.setCellValue(purchase.getOrderId());
+            cellTemp.setCellStyle(style);
+            Integer status = purchase.getOrderStatus();
+            String orderStatus = "";
+            orderStatus = getOrderStatusInExcel(status, orderStatus);
+            cellTemp = row.createCell(2);
+            cellTemp.setCellValue(orderStatus);
+            cellTemp.setCellStyle(style);
+            cellTemp = row.createCell(3);
+            cellTemp.setCellValue("￥" + purchase.getOrderPrice());
+            cellTemp.setCellStyle(style);
+            cellTemp = row.createCell(4);
+            cellTemp.setCellValue(purchase.getOrderCreateTime() == null ? "" : StringUtil.fomateData(purchase.getOrderCreateTime(), "yyyy-MM-dd HH:mm:ss"));
+            cellTemp.setCellStyle(style);
+            cellTemp = row.createCell(5);
+            cellTemp.setCellValue(purchase.getOrderPaymentTime() == null ? "" : StringUtil.fomateData(purchase.getOrderCreateTime(), "yyyy-MM-dd HH:mm:ss"));
+            cellTemp.setCellStyle(style);
+            Integer paymentMethod = purchase.getOrderPaymentMethod();
+            String payment = "";
+            if (paymentMethod == Constant.PaymentStatusConfig.ALIPAY) {
+                payment = Constant.PaymentMsgConfig.ALIPAY;
+            } else if (paymentMethod == Constant.PaymentStatusConfig.WECHAT) {
+                payment = Constant.PaymentMsgConfig.WECHAT;
+            }
+            cellTemp = row.createCell(6);
+            cellTemp.setCellValue(payment);
+            cellTemp.setCellStyle(style);
+            cellTemp = row.createCell(7);
+            cellTemp.setCellValue(purchase.getOrderPaymentNum());
+            cellTemp.setCellStyle(style);
+        }
+        //输出Excel文件
+        outputExcel(request, response, wb);
+    }
+    //输出Excel文件
+    private void outputExcel(HttpServletRequest request, HttpServletResponse response, HSSFWorkbook wb) {
+        OutputStream output = null;
+        try {
+            output = response.getOutputStream();
+            response.reset();
+            String agent = request.getHeader("USER-AGENT").toLowerCase();
+            response.setContentType("application/vnd.ms-excel");
+            String fileName = "订单统计" + System.currentTimeMillis();
+            String codedFileName = java.net.URLEncoder.encode(fileName, "UTF-8");
+            if (agent.contains("firefox")) {
+                response.setCharacterEncoding("utf-8");
+                response.setHeader("content-disposition", "attachment;filename=" + new String(fileName.getBytes(), "ISO8859-1") + ".xls");
+            } else {
+                response.setHeader("content-disposition", "attachment;filename=" + codedFileName + ".xls");
+            }
+            wb.write(output);
+            output.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //导出订单Excel订单状态翻译
+    private String getOrderStatusInExcel(Integer status, String orderStatus) {
+        if (status == Constant.OrderStatusConfig.PAYMENT) {
+            orderStatus = Constant.OrderMessageConfig.PAYMENT;
+        } else if (status == Constant.OrderStatusConfig.PENDING_SHIP) {
+            orderStatus = Constant.OrderMessageConfig.PENDING_SHIP;
+        } else if (status == Constant.OrderStatusConfig.ISSUE_SHIP || status == Constant.OrderStatusConfig.CONFIRM_RECEIVED) {
+            orderStatus = Constant.OrderMessageConfig.ISSUE_SHIP;
+        } else if (status == Constant.OrderStatusConfig.RECEIVED) {
+            orderStatus = Constant.OrderMessageConfig.RECEIVED;
+        } else if (status == Constant.OrderStatusConfig.REJECT) {
+            orderStatus = Constant.OrderMessageConfig.REJECT;
+        } else if (status == Constant.OrderStatusConfig.REFUNDED) {
+            orderStatus = Constant.OrderMessageConfig.REFUNDED;
+        } else if (status == Constant.OrderStatusConfig.CANCEL_ORDER) {
+            orderStatus = Constant.OrderMessageConfig.CANCEL_ORDER;
+        } else if (status == Constant.OrderStatusConfig.PAYMENT_CANCEL_ORDER) {
+            orderStatus = Constant.OrderMessageConfig.PAYMENT_CANCEL_ORDER;
+        }
+        return orderStatus;
     }
 }
