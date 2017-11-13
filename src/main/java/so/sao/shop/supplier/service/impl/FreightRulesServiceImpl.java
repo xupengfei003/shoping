d4bcfb83b3aa6545fce988c0881837056495ea42
zhenhai.zheng <zhenhai.zheng@sao.so>
2017-11-13 18:07:51 +0800

    收货地址与商家配送规则更改

--- src/main/java/so/sao/shop/supplier/service/impl/FreightRulesServiceImpl.java
+++ src/main/java/so/sao/shop/supplier/service/impl/FreightRulesServiceImpl.java
@@ -192,7 +192,6 @@ public class FreightRulesServiceImpl implements FreightRulesService {
         return 0;
     }
 
-
     /**
      * 地址匹配 ---配送范围与下单收货地址的匹配
      *
@@ -211,11 +210,15 @@ public class FreightRulesServiceImpl implements FreightRulesService {
                 if (Ognl.isNotEmpty(dbDistrict) && dbDistrict.equals(district)) {//匹配区
                     return freightRules;
                 }
-                if (Ognl.isNotEmpty(dbCity) && dbCity.equals(city)) {//匹配市
-                    rulesCity = freightRules;
-                }
-                if (Ognl.isNotEmpty(dbProvince) && dbProvince.equals(province)) {//匹配省
-                    rulesProvince = freightRules;
+                if (Ognl.isEmpty(dbDistrict) || Ognl.isNull(dbDistrict)){//匹配全部区
+                    if (Ognl.isNotEmpty(dbCity) && dbCity.equals(city)) {//匹配市
+                        rulesCity = freightRules;
+                    }
+                    if (Ognl.isNull(dbCity) || Ognl.isEmpty(dbCity)){//匹配全部市
+                        if (Ognl.isNotEmpty(dbProvince) && dbProvince.equals(province)) {//匹配省
+                            rulesProvince = freightRules;
+                        }
+                    }
                 }
             }
 
