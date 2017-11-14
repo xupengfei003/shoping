af7c972cace61be24918dcfb42cbba665eedbeba
zhenhai.zheng <zhenhai.zheng@sao.so>
2017-11-14 18:33:30 +0800

    此类中缺少BigDecimal的引包

--- src/main/java/so/sao/shop/supplier/service/external/impl/CouponServiceImpl.java
+++ src/main/java/so/sao/shop/supplier/service/external/impl/CouponServiceImpl.java
@@ -14,6 +14,7 @@ import so.sao.shop.supplier.pojo.Result;
 import so.sao.shop.supplier.service.external.CouponService;
 import so.sao.shop.supplier.util.PageTool;
 
+import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
 
